5d4e989f2075aafff6eceae306460d7925ea8c30
Ahmed Fathy <jodat.ahmedsyed@gmail.com>
2025-06-22 18:24:34 +0300

    add header at getData

--- src/helperFunc/getData.js
+++ src/helperFunc/getData.js
@@ -2,7 +2,15 @@ import axios from "axios";
 
 export const getData = async () => {
     try {
-        const data = await axios.get("../../../public/Data.json")
+        const data = await axios.get("../../../public/Data.json", {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            withCredentials: false,
+
+        })
         const response = data.data
         return response
     } catch (error) {
